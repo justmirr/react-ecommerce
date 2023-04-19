dc4bfa48cefb99b342132b078cec4c6e5caae9cd
Mir <75358919+justmirr@users.noreply.github.com>
2023-04-19 11:49:14 +0530

    Update Banner.js

--- client/src/components/maincomp/Banner.js
+++ client/src/components/maincomp/Banner.js
@@ -1,26 +1,48 @@
 import React from 'react'
-import './banner.css';
 import Carousel from 'react-material-ui-carousel'
+import "./banner.css";
 
-const Data = [
-    "https://images-eu.ssl-images-amazon.com/images/G/31/img2020/img21/GWUnrec_BOB/WA3000._CB592962828_.jpg",
-    "https://m.media-amazon.com/images/I/51ovs76vmtL._SX3000_.jpg"
-];
+const data = [
+    "https://rukminim1.flixcart.com/flap/1680/280/image/1defb861e409319b.jpg?q=50",
+    " https://rukminim1.flixcart.com/flap/1680/280/image/685712c6cefb3c02.jpg?q=50",
+    "https://rukminim1.flixcart.com/flap/1680/280/image/8d4150cc4f3f967d.jpg?q=50",
+    "https://rukminim1.flixcart.com/flap/1680/280/image/685712c6cefb3c02.jpg?q=50"
+]
+
 
 const Banner = () => {
-  return (
-    <Carousel className='carousel'>
-        {
-            Data.map((imag, i) => {
-                return (
-                    <>
-                        <img src={imag} alt="" className='banner_img'/>
-                    </>
-                )
-            })
-        }
-    </Carousel>
-  )
+    return (
+        <>
+
+            <Carousel
+                className="carousel"
+                autoPlay={true}
+                animation="slide"
+                indicators={false}
+                navButtonsAlwaysVisible={true}
+                cycleNavigation={true}
+                navButtonsProps={{
+                    style: {
+                        background: "#c0ebcb",
+                        color: "#252525",
+                        borderRadius: 0,
+                        marginTop: -22,
+                        height: "104px",
+                    }
+                }}>
+                {
+                    data.map((imag, i) => {
+                        return (
+                            <>
+                                <img src={imag} alt="img" key={i} className="banner_img" />
+                            </>
+                        )
+                    })
+                }
+
+            </Carousel>
+        </>
+    )
 }
 
-export default Bannerexport default Banner;
+export default Banner;
